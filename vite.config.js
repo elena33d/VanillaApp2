2696b90cf389fa726e37a844a6ab3b07e6233061
elena <o.didenko@student.bachelor-goit.com.ua>
2026-02-05 02:48:13 +0100

    F

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import fullReload from 'vite-plugin-full-reload'
 
 export default defineConfig({
   root: 'src',
+  publicDir: '../public',
   plugins: [
     injectHTML(),
     fullReload(['**/*.html'])
@@ -11,5 +12,16 @@ export default defineConfig({
   build: {
     outDir: '../docs',
     emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split('.').at(1);
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'img';
+          }
+          return `assets/${extType}/[name]-[hash][extname]`;
+        }
+      }
+    }
   }
 })
